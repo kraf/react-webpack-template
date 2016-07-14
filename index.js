7da6472ae3eea8e8bbdaae863ac3a89862ac0b5b
kraf <filip.krasnianski@gmail.com>
2016-07-14 18:59:56 +0200

    Update versions; add firebase

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var firebase = require('firebase/app');
+require('firebase/database');
+
+var config = {
+    apiKey: "*",
+    authDomain: "*",
+    databaseURL: "*",
+    storageBucket: "*",
+};
+firebase.initializeApp(config);
+
+window.firebase = firebase;
+
 ReactDOM.render(<h1>Hello React!</h1>, document.getElementById('root'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,19 @@
 module.exports = {
-  entry: './index',
-  output: {
-    filename: 'browser-bundle.js'
-  },
-  devtool: 'source-map',
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react']
-        }
-      },
-    ]
-  }
+    entry: './index',
+    output: {
+        filename: 'browser-bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+        ]
+    }
 };
