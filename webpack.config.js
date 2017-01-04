a6db4b9ba2e16fdcfd2031eede11c36b829040b1
Filip Krasnianski <kraf@posteo.de>
2017-01-04 09:37:17 +0100

    Use dev-server and improve webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,38 @@
+const webpack = require('webpack');
+
+const production = process.env.NODE_ENV === 'production';
+
 module.exports = {
     entry: './index.jsx',
     output: {
-        filename: 'browser-bundle.js'
+        filename: 'bundle.js'
     },
-    devtool: 'source-map',
+    devtool: production ? 'sourcemap' : 'eval',
     module: {
         loaders: [
             {
                 test: /\.jsx?$/,
                 loader: 'babel-loader',
-                exclude: /node_modules/,
-                query: {
-                    presets: ['es2015', 'react']
-                }
+                exclude: /node_modules/
             },
+            {
+                test: /\.css?$/,
+                loaders: ['style-loader', 'css-loader?-url']
+            },
+            {
+                test: /\.scss?$/,
+                loaders: ['style-loader', 'css-loader', 'sass-loader']
+            }
         ]
-    }
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: JSON.stringify(process.env.NODE_ENV)
+            }
+        })
+    ]
 };
